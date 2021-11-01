6044273970df65fedaee6ee13d95c5228ca52d5b
dvarx <dvarx@ethz.ch>
2021-11-01 06:39:36 +0100

    moved functionality into classes

--- magcalc.py
+++ magcalc.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 import os
 from math import floor,sqrt,pi,sin,cos
 from numpy.linalg import norm
-from mayavi import mlab
 from scipy.interpolate import RegularGridInterpolator
+
 
 def linidx2volidx(n):
     global N
@@ -43,6 +43,8 @@ def get_magfield_data(filenames,N):
                         coord_strs=split_lines[n][48:-1].split(",")
                         coordinates[coordcounter,:]=np.array([float(coord_strs[0]),float(coord_strs[1]),float(coord_strs[2])])
                         coordcounter+=1
+                        if coordcounter>=N**3-1:
+                            break
 
             #the lines contain the actual simulation data
             if linecounter>4:
@@ -62,35 +64,63 @@ def get_magfield_data(filenames,N):
 
         fptr.close()
     
+    
+
     return (Bs,coordinates)
+
+
+class magnetic_system:
+    def __init__(self,calibration_data,calibration_type,N):
+        if(calibration_type=="comsol_csv"):
+            self.Bs,self.coordinates=get_magfield_data(calibration_data,N)
+            #extract the coordinate values of the grid
+            self.xcoords=list(set(list(self.coordinates[:,0])))
+            self.xcoords.sort()
+            self.xmin=min(self.xcoords)
+            self.xmax=max(self.xcoords)
+            self.ycoords=list(set(list(self.coordinates[:,1])))
+            self.ycoords.sort()
+            self.ymin=min(self.xcoords)
+            self.ymax=max(self.xcoords)
+            self.zcoords=list(set(list(self.coordinates[:,2])))
+            self.zcoords.sort()
+            self.zmin=min(self.xcoords)
+            self.zmax=max(self.xcoords)
+            self.BactInterpolator=RegularGridInterpolator((self.xcoords,self.ycoords,self.zcoords),self.Bs)
+        elif(calibration_type=="pickle"):
+            raise Exception("Method not implemented")
+        else:
+            raise ValueError("calibration data need to be either comsol generated csv file or pickled calibration data")
+
+    def getBact(self,pos):
+        return self.BactInterpolator(pos)
+
+    def plot_yz_view(self,currents):
+        N=20
+        zs=np.linspace(self.zmin,self.zmax,N)
+        ys=np.linspace(self.zmin,self.zmax,N)
+        vals=np.zeros((N,N))
+        for i in range(0,N):
+            for j in range(0,N):
+                vals[i,j]=norm((self.BactInterpolator((0,ys[j],zs[i]))).dot(currents))
+        plt.imshow(vals)
+        plt.show()
+
+
 
 if __name__=="__main__":
     # extract field data
-    os.chdir("C:\\Users\\dvarx\\src\\magcalc\\python_comps")
-    N=13                        # no field samples in each direction
-    filenames=["bxs_wcore.csv","bys_wcore.csv","bzs_wcore.csv"]
-    (Bs,coordinates)=get_magfield_data(filenames,N)
+    os.chdir(os.getcwd()+"\\python_comps")
+    N=13  # no field samples in each direction
+    filenames=["bxs_wocore.csv","bys_wocore.csv","bzs_wocore.csv"]
+
+    system=magnetic_system(["bxs_wocore.csv","bys_wocore.csv","bzs_wocore.csv"],"comsol_csv",N)
 
     #calculate field
-    #currents=np.array([10,10,10,10,10,10])          #opposing fields
-    currents=np.array([10,10,10,-10,-10,-10])        #additive fields
-    Bsum=Bs.dot(currents)
+    currents=np.array([10,10,10,-10,-10,-10])           #opposing fields
+    #currents=np.array([10,10,10,-10,-10,-10])          #additive fields
+    Bsum=system.getBact((0,0,0)).dot(currents)
 
-    #extract the coordinate values of the grid
-    xcoords=list(set(list(coordinates[:,0])))
-    xcoords.sort()
-    ycoords=list(set(list(coordinates[:,1])))
-    ycoords.sort()
-    zcoords=list(set(list(coordinates[:,2])))
-    zcoords.sort()
-    #define interpolator function for magnetic fields
-    def getInterpolator(B):
-        return RegularGridInterpolator((xcoords,ycoords,zcoords),B)
-
-    field_int=getInterpolator(Bsum)
-
-    mlab.quiver3d(coordinates[:,0],coordinates[:,1],coordinates[:,2],Bsum[:,:,:,0].flatten(),Bsum[:,:,:,1].flatten(),Bsum[:,:,:,2].flatten())
-
-    mlab.show()
+    system.plot_yz_view(currents)
 
     print("finished")
