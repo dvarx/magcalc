c456978726d726a6d00ea347e3cb04985fe4bc02
dvarx <59966971+dvarx@users.noreply.github.com>
2021-11-01 16:19:24 +0100

    initial mpi signal computation

--- magcalc.py
+++ magcalc.py
@@ -1,10 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-from math import floor,sqrt,pi,sin,cos
+from math import floor,sqrt,pi,sin,cos,gcd,sinh,cosh
 from numpy.linalg import norm
 from scipy.interpolate import RegularGridInterpolator
+from scipy.fft import fft
 
+mu0=4*pi*1e-7
+kb=1.38e-23
+Tp=309
+NA=6.022e23
+
+def langevin(x):
+    if x==0:
+        return 0
+    else:
+        return cosh(x)/sinh(x)-1/x
 
 def linidx2volidx(n):
     global N
@@ -68,6 +79,11 @@ def get_magfield_data(filenames,N):
 
     return (Bs,coordinates)
 
+def lcm(numbers):
+    lcm = 1
+    for i in numbers:
+        lcm = lcm*i//gcd(lcm, i)
+    return lcm
 
 class magnetic_system:
     def __init__(self,calibration_data,calibration_type,N):
@@ -106,21 +122,122 @@ class magnetic_system:
         plt.imshow(vals)
         plt.show()
 
-
-
 if __name__=="__main__":
     # extract field data
-    os.chdir(os.getcwd()+"\\python_comps")
+    os.chdir(os.getcwd()+"\\comsol")
     N=13  # no field samples in each direction
-    filenames=["bxs_wocore.csv","bys_wocore.csv","bzs_wocore.csv"]
+    filenames=["\\comsol\\bxs_wocore.csv","\\comsol\\bys_wocore.csv","\\comsol\\bzs_wocore.csv"]
 
     system=magnetic_system(["bxs_wocore.csv","bys_wocore.csv","bzs_wocore.csv"],"comsol_csv",N)
 
     #calculate field
-    currents=np.array([10,10,10,-10,-10,-10])           #opposing fields
-    #currents=np.array([10,10,10,-10,-10,-10])          #additive fields
-    Bsum=system.getBact((0,0,0)).dot(currents)
+    #currents=np.array([10,10,10,-10,-10,-10])           #opposing fields
+    #currents=np.array([10,10,10,-10,-10,0])          #additive fields
+    #Bsum=system.getBact((0,0,0)).dot(currents)
 
-    system.plot_yz_view(currents)
+    #system.plot_yz_view(currents)
+
+    ###########################
+    #mpi simulation
+    ###########################
+
+    #compute the currents for the six coils
+    #-------------------------------------------------------------------------------------
+    f0=5e3      #the highest frequency in the system
+    Nlcm=lcm([7,8,9])
+    dT=1/f0/7
+    T1=9*dT
+    T2=8*dT
+    T3=7*dT
+    Trep=Nlcm*dT
+    f1=1/T1
+    f2=1/T2
+    f3=1/T3
+    Nsamples=Nlcm*100
+    ts=np.linspace(0,Trep,Nsamples)
+    dt=Trep/(Nsamples)
+    iampl=6
+    i1s=iampl*np.sin(2*pi*f1*ts)
+    i2s=iampl*np.sin(2*pi*f2*ts)
+    i3s=iampl*np.sin(2*pi*f3*ts)
+    plt.figure(1)
+    ax = plt.axes(projection='3d')
+    ax.plot3D(i1s,i2s,i3s)
+
+    #define the concentration function & magnetization function
+    #-------------------------------------------------------------------------------------
+    Msat=6.5e3              #saturation magnetization [A/m]
+    V=4*pi/3*(30e-9/2)**3   #particle volume [m^3]
+    m=Msat*V                #maximum magnetic dipole moment of particle [Am^2]
+    beta=mu0*m/(kb*Tp)
+    #c=Msat/m                #particle concentration for unity iron concentration
+    c=116*NA/(1e-3)         #166mol/l as reported by Panagiotopoulos1 et al
+    #computes the magnetization M[A/m] dependent upon external magnetic field H[A/m] and iron volumetric concentration [m^-3]
+    alpha=100  #unknown correction factor
+    def M(c,H):
+        global mu0,m,beta
+        return c*m*langevin(alpha*beta*H)
+    Bs=np.linspace(0,20e-3,100)
+    Ms=np.zeros(Bs.shape)
+    for i in range(0,len(Bs)):
+        Ms[i]=M(c,Bs[i]/mu0)
+    plt.figure()
+    plt.plot(1e3*Bs,Ms)
+    plt.xlabel("B [mT]")
+    plt.show()
+
+    #define the phantom concentration function
+    #-------------------------------------------------------------------------------------
+
+    #main simulation
+    #-------------------------------------------------------------------------------------
+    usint=np.zeros(len(ts))
+    usig=np.zeros(len(ts))
+    Bs=np.zeros(len(ts))
+    usint[0]=0
+    usig[0]=0
+    for i in range(1,len(ts)):
+        t=i*dt
+        currents=np.array([i1s[i],i2s[i],i3s[i],i1s[i],i2s[i],i3s[i]])
+
+        #compute the magnetic field at the origin
+        Bsum=system.getBact((0,0,0)).dot(currents)
+
+        #compute the saturation factor (value of langevin function)
+        Bs[i]=norm(Bsum)
+        usint[i]=langevin(alpha*beta*norm(Bsum)/mu0)
+        usig[i]=(usint[i]-usint[i-1])/dt
+
+
+    fig, axs = plt.subplots(3,sharex=True)
+    fig.suptitle('MPI Signals')
+    axs[0].plot(ts*1e3,usint)
+    axs[0].set_ylabel(r"$\mathcal{L}(M)=\frac{M}{M_{sat}}$")
+    axs[0].grid()
+    axs[1].plot(ts*1e3,usig)
+    axs[1].grid()
+    axs[1].set_ylabel(r"$\frac{d}{dt}\mathcal{L}(t)$")
+    axs[2].plot(ts*1e3,Bs)
+    axs[2].grid()
+    axs[2].set_ylabel(r"$|B(\vec{r_{P}},t)|$")
+    plt.show()
+    
+    #compute and plot fft
+    fig, axs = plt.subplots(2,sharex=True)
+    #compute current ffts
+    i1s_fft=fft(i1s)
+    i2s_fft=fft(i2s)
+    i3s_fft=fft(i3s)
+    fig.suptitle("Harmonic Contents")
+    Nfft=floor(Nsamples/2)
+    axs[0].plot(abs(i1s_fft[:Nfft]+i2s_fft[:Nfft]+i3s_fft[:Nfft]))
+    axs[0].set_ylabel("Current Signal")
+    #compute
+    usig_fft=fft(usig)
+    axs[1].plot(abs(usig_fft[:Nfft]))
+    axs[1].set_ylabel("Particle Signal")
+    plt.show()
+
+    #compute the magnetic field for the pickup coil (sensitivity function)
 
     print("finished")
